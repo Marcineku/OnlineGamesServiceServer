891ff31af0e4678c5f15aa8f03393888f8d7520a
Mateusz Romaniuk <37747987+mr-mateusz@users.noreply.github.com>
2018-11-18 14:24:32 +0100

    Test: Added example rest controller (helloWorld)

---
+++ src/main/java/pl/edu/wat/wcy/pz/project/server/hello/controller/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package pl.edu.wat.wcy.pz.project.server.hello.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
